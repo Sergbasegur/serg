b170958ea75ec8c24e39f0f490901c2ac5861b4c
Denis <denis1993@yandex.ru>
2022-11-28 15:43:38 +0300

    Добавлена задача № 1

--- Python_1/denis_1/lesson.py
+++ Python_1/denis_1/lesson.py
@@ -1,2 +1,21 @@
-n = int(input())
-print(*[i for i in range(-n, n + 1)])
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр
+
+vech = float(input('введите вещественное число - '))
+
+while vech != int(vech):
+    vech *= 10
+sum = 0
+per = 1
+while vech > 0:
+    d = vech % 10
+    sum += d
+    vech //=10
+    
+print(sum)
+
+
+
+
+
+
