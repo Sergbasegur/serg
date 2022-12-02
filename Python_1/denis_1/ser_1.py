d2ffff05609882cc262d6cbcc54ecc6dd69543fb
Denis <denis1993@yandex.ru>
2022-12-02 12:34:38 +0300

    Перемешивание списка

--- Python_1/denis_1/ser_1.py
+++ Python_1/denis_1/ser_1.py
@@ -1,7 +1,12 @@
-def f(x):
-    if x == 1:
-        return 'Целое'
-    elif x == 2.3:
-        return 23
-    else:
-        return
+from random import randint
+def mix(lst):
+    for i in range(len(lst)):
+        new_i = randint(0, len(lst) - 1)
+        lst[i], lst[new_i] = lst[new_i], lst[i]
+       # tmp = lst[i]
+       # lst[i] = lst[new_i]
+       # lst[new_i] = tmp
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+mix(lst)
+print(lst)
