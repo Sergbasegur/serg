95c30a1da7fdd6a7f73d843c34de21f37a325f17
Denis <denis1993@yandex.ru>
2022-12-07 13:16:53 +0300

    Сформировать файл содержащий сумму многочленов

--- Python_1/denis_1/lesson_14.py
+++ Python_1/denis_1/lesson_14.py
@@ -3,22 +3,22 @@
 # Задача - сформировать файл, содержащий сумму многочленов.
 # Коэффициенты могут быть как положительными, так и отрицательными.
 # Степени многочленов могут отличаться.
-with open('poly_1.txt', 'w', encoding='utf-8') as file:
-    file.write('2*x^2 + 5*x^5')
-with open('poly_2.txt', 'w', encoding='utf-8') as file:
-    file.write('23*x^4 + 9*x^6')
+with open('file_1.txt', 'w', encoding='utf-8') as file:
+    file.write('3*x^3 + 6*x^7')
+with open('file_2.txt', 'w', encoding='utf-8') as file:
+    file.write('10*x^5 + 8*x^4')
 
-with open('poly_1.txt','r') as file:
-    poly_1 = file.readline()
-    list_of_poly_1 = poly_1.split()
+with open('file_1.txt','r') as file:
+    mn_1 = file.readline()
+    poligam_1 = mn_1.split()
 
 
-with open('poly_2.txt','r') as file:
-    poly_2 = file.readline()
-    list_of_poly_2 = poly_2.split()
+with open('file_2.txt','r') as file:
+    mn_2 = file.readline()
+    poligam_2 = mn_2.split()
 
-print(f'{list_of_poly_1} + {list_of_poly_2}')
-sum_poly = list_of_poly_1 + list_of_poly_2
+print(f'{poligam_1} + {poligam_2}')
+sum_poly = poligam_1 + poligam_2
 
-with open('sum_poly.txt', 'w', encoding='utf-8') as file:
-    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
+with open('sum_poligam.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{poligam_1} + {poligam_2}')
