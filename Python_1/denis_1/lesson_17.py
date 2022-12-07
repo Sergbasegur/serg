48aace900ff42e1d7f1d9af39af04e6087067209
Denis <denis1993@yandex.ru>
2022-12-07 15:25:17 +0300

    алгоритм кодировки и декодировки

---
+++ Python_1/denis_1/lesson_17.py
@@ -0,0 +1,36 @@
+ # Задача 
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия
+#  и восстановления данных.
+
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+# aaaasssdddwwwwwwwwwwwweeeffffff -> 4a3s3d9w3w3e6f
+# 4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff
+def coding(text):
+    count = 1
+    res = ''
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + text[i]
+            count = 1
+    if count > 1 or (text[len(text)-2] != text[-1]):
+        res = res + str(count) + text[-1]
+    return res
+
+def decoding(text):
+    number = ''
+    res = ''
+    for i in range(len(text)):
+        if not text[i].isalpha():
+            number += text[i]
+        else:
+            res = res + text[i] * int(number, base=36)
+            number = ''
+    return res
+
+
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
