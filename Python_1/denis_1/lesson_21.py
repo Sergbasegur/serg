6b1c5822bab954f1170f2def9509d65f480bf2e0
Denis <denis1993@yandex.ru>
2022-12-13 10:46:21 +0300

    Ускоренная обработка данных - enemerate

---
+++ Python_1/denis_1/lesson_21.py
@@ -0,0 +1,23 @@
+#Задайте список из вещественных чисел.
+#  Напишите программу, которая найдёт разницу между
+#  максимальным и минимальным значением дробной части элементов.
+
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+n = [1.1, 1.2, 3.1, 5, 10.01]
+   
+index_min = index_max = 0
+value_min = value_max = n[0]
+
+
+for i, a in enumerate(n[1:], 1):
+    z = 0
+    if a < value_min:
+        value_min = a
+        index_min = i
+    if a > value_max:
+        value_max = a
+        index_max = i
+z = n[index_max] - n[index_min] 
+print(f'{n} --> {z}')
