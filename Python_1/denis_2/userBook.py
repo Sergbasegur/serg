e5c35d9c7ada6ca454ff8226dc51c05f81bf3b2a
Denis <denis1993@yandex.ru>
2022-12-19 12:50:14 +0300

    Добавление файла просмотра контактов

--- Python_1/denis_2/userBook.py
+++ Python_1/denis_2/userBook.py
@@ -10,11 +10,14 @@ def start():
 
 def menu():
     what_to_do = 'Выберите соответствующую цифру в меню:'
-    new_book = '0. Создать новую книгу или очистить существующую'
+    to_exit = '0. Выход'
     addContact = '1. Добавить новый контакт'
-    delete_contact = '2. Удалить контакт'
-    view_all_contact = '3. Показать все контакты'
-    export_to_file = '4. Экспортировать контакты в файл'
-    to_exit = '5. Выход'
-    print(f'{what_to_do}\n\n{new_book}\n{addContact}\n{delete_contact}\n{view_all_contact}\n{export_to_file}\n{to_exit}')
+    create_contact= '2. Добавить новый контакт'
+    exportFile = '3. Экспортировать контакты в файл'
+    delete_contact = '4. Удалить контакт'
+
+    view_all_contact = '5. Показать все контакты'
+   
+    
+    print(f'{what_to_do}\n{to_exit}\n{addContact}\n{create_contact}\n{exportFile}\n{delete_contact}\n{view_all_contact}')
     return check.number_check()
---
+++ Python_1/denis_2/viewContact.py
@@ -0,0 +1,14 @@
+import json
+import controller
+
+
+path_to_db = 'basa_d.json'
+
+
+def view_all_contacts():
+
+    with open(path_to_db, 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+        for i in range(0, len(data)):
+            print(data[i])
+    controller.user_controller()
