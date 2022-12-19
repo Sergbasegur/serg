3dbdb28fe5c4bc01752f5ed9b040185011e3d365
Denis <denis1993@yandex.ru>
2022-12-19 10:40:00 +0300

    Добавление контактов

---
+++ Python_1/denis_2/addContact.py
@@ -0,0 +1,28 @@
+import json
+import controller
+
+
+def create():
+    json_data = []
+    with open('db.json', 'w') as file:
+        file.write(json.dumps(json_data, indent=2, ensure_ascii=False))
+    controller.user_choice()
+
+
+def add():
+    name = input("Введите имя: ")
+    surname = input('Введите Фамилию: ')
+    phone = input('Введите номер телефона: ')
+    comment = input('Введите коментарий: ')
+    base_data = {
+        "Name": name,
+        "Surname": surname,
+        "Phone number": phone,
+        "Comment": comment,
+    }
+    data = json.load(open("db.json"))
+    data.append(base_data)
+    with open("db.json", "w") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+    print('\nКонтакт успешно добавлен\n')
+    controller.user_choice()
