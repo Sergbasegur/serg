77a5bf9aac59ec72da0ced3eaa41e2d30c7e5d92
Denis <denis1993@yandex.ru>
2022-12-19 11:33:27 +0300

    Добавление экспорта файла

--- Python_1/denis_2/controller.py
+++ Python_1/denis_2/controller.py
@@ -1,19 +1,21 @@
 import addContact as ac
-import delete_contact as dc
-import export_in_file as eif
+import exportFile as f
 import userBook as ui
 def user_controller():
 
     num = ui.menu()
     if num < 0 or choice_num > 7:
         print('\nОшибка ввода!\n\nЧисло должно соответствовать пункту меню!\n')
-        user_choice()
-    elif num == 0:
-        ac.create()
+        user_controller()
     elif num == 1:
+        ac.create()
+    elif num == 2:
         ac.add()
+    elif num == 3:
+        f.export_txt()
     
-    elif choice_num == 7:
+    
+    elif num == 0:
         print('\nСпасибо что пользовались нашим приложением!\n\nДо новых встреч!')
         exit()
 
--- Python_1/denis_2/userBook.py
+++ Python_1/denis_2/userBook.py
@@ -1,5 +1,5 @@
 import check
-from export_in_file import export_txt
+from exportFile import export_txt
 
 
 def start():
@@ -9,12 +9,12 @@ def start():
 
 
 def menu():
-    what_to_do = 'Что будем делать? Выберите соответствующую цифру в меню:'
+    what_to_do = 'Выберите соответствующую цифру в меню:'
     new_book = '0. Создать новую книгу или очистить существующую'
     addContact = '1. Добавить новый контакт'
     delete_contact = '2. Удалить контакт'
     view_all_contact = '3. Показать все контакты'
     export_to_file = '4. Экспортировать контакты в файл'
     to_exit = '5. Выход'
-    print(f'{what_to_do}\n\n{new_book}\n{new_contact}\n{delete_contact}\n{view_all_contact}\n{export_to_file}\n{to_exit}')
-    return chek.digit_check()
+    print(f'{what_to_do}\n\n{new_book}\n{addContact}\n{delete_contact}\n{view_all_contact}\n{export_to_file}\n{to_exit}')
+    return check.number_check()
