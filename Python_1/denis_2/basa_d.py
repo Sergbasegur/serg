d787a9977c789a9d3aba6d6dc2800ad62111cb7b
Denis <denis1993@yandex.ru>
2022-12-19 12:01:31 +0300

    Добавление файла базы

---
+++ Python_1/denis_2/basa_d.py
@@ -0,0 +1,50 @@
+[
+    {
+        "Name": "Сергей",
+        "Surname": "Лыков",
+        "Phone number": "7 950 245",
+        "Comment": "друг"
+    },
+    {
+        "Name": "Антон",
+        "Surname": "Смирнов",
+        "Phone number": "7 950 345",
+        "Comment": "брат"
+    },
+    {
+        "Name": "Лена",
+        "Surname": "Смирнова",
+        "Phone number": "7 969 678",
+        "Comment": "сестра"
+    },
+    {
+        "Name": "Денис",
+        "Surname": "Петров",
+        "Phone number": "8-756 455",
+        "Comment": "компаньон"
+    },
+    {
+        "Name": "Наташа",
+        "Surname": "Лесова",
+        "Phone number": "03",
+        "Comment": "скорая помощь"
+    },
+    {
+        "Name": "Петро",
+        "Surname": "Лушайло",
+        "Phone number": "8 987 111",
+        "Comment": "сосед"
+    },
+    {
+        "Name": "Рома",
+        "Surname": "Степанов",
+        "Phone number": "8 945 33213",
+        "Comment": "дружбан"
+    },
+    {
+        "Name": "Игорь",
+        "Surname": "колесов",
+        "Phone number": "8 767 3215",
+        "Comment": "партнёр по компании"
+    }
+]
