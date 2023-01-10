0e2b4afcd16be30a4fd498a51ad3c48f91bd6b68
Denis <denis1993@yandex.ru>
2023-01-10 15:06:01 +0300

    добавлен файл requirements.txt

--- PyPi/calc/main.py
+++ PyPi/calc/main.py
@@ -1,6 +1,8 @@
 import logging
 from bot import TOKEN
+#Для установки  python-telegram-bot --версия 13.14 
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler)
+# Для установки файла requirements.txt    ----  pip freeze > requirements.txt
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
